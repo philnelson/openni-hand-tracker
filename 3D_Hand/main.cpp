3ee9607a31aeff894d90317dd438e8f40cb2f250
maurilio di cicco <mauriliodc@helvete.homenet.telecomitalia.it>
2012-03-03 18:56:15 +0100

    Initial Commit

---
+++ 3D_Hand/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  3D_Hand
+//
+//  Created by maurilio di cicco on 03/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
