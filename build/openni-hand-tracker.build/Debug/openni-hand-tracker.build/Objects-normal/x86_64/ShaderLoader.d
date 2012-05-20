95567596848e3b26d71f47c4db47dcb013dc7c9d
Thor <maurilio.dicicco@gmail.com>
2012-05-20 03:10:06 +0200

    progetto xcode

---
+++ build/openni-hand-tracker.build/Debug/openni-hand-tracker.build/Objects-normal/x86_64/ShaderLoader.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mauriliodc/Desktop/openni-hand-tracker/openni-hand-tracker/ShaderLoader.cpp \
+  /Users/mauriliodc/Desktop/openni-hand-tracker/openni-hand-tracker/ShaderLoader.h
